b3f0291582e2f6a7e482c5b987f02e364dabf68c
Miguel de Dios <migueldedios@google.com>
2018-11-01 10:31:33 -0700

    asoc: msm-dai-q6-v2: Mark msm-dai-q6 drivers as sync probe.
    Bug: 115776306
    Bug: 77146523
    Change-Id: I31d533a62e2dbfbc27aea8bf8794cf4c5d201c9a
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    (cherry picked from commit 704a0a22b6e71d717b43fbc9aae4d8c610d7d5cd)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- techpack/audio/asoc/msm-dai-q6-v2.c
+++ techpack/audio/asoc/msm-dai-q6-v2.c
@@ -6468,6 +6468,7 @@ static struct platform_driver msm_dai_q6_dev = {
 		.name = "msm-dai-q6-dev",
 		.owner = THIS_MODULE,
 		.of_match_table = msm_dai_q6_dev_dt_match,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
@@ -6505,6 +6506,7 @@ static struct platform_driver msm_dai_q6 = {
 		.name = "msm-dai-q6",
 		.owner = THIS_MODULE,
 		.of_match_table = msm_dai_q6_dt_match,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
@@ -6832,6 +6834,7 @@ static struct platform_driver msm_dai_tdm_q6 = {
 		.name = "msm-dai-tdm",
 		.owner = THIS_MODULE,
 		.of_match_table = msm_dai_tdm_dt_match,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
@@ -10487,6 +10490,7 @@ static struct platform_driver msm_dai_q6_tdm_driver = {
 		.name = "msm-dai-q6-tdm",
 		.owner = THIS_MODULE,
 		.of_match_table = msm_dai_q6_tdm_dev_dt_match,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 

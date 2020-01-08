666356b3f63e5c8e14074a98a1ddd9a09c245f34
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2020-01-08 10:30:04 +0530

    sched: Add support to spread tasks
    If sysctl_sched_prefer_spread is enabled, then tasks would be freely
    migrated to idle cpus within same cluster to reduce runnables.
    
    By default, the feature is disabled.
    User can trigger feature with:
       echo 1 > /proc/sys/kernel/sched_prefer_spread
    	Aggressively spread tasks with in little cluster.
       echo 2 > /proc/sys/kernel/sched_prefer_spread
    	Aggressively spread tasks with in little cluster as well as
    	big cluster, but not between big and little.
    
    Change-Id: I0a4d87bd17de3525548765472e6f388a9970f13c
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -547,6 +547,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_updown_migrate_handler,
 	},
 	{
+		.procname	= "sched_prefer_spread",
+		.data		= &sysctl_sched_prefer_spread,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &two,
+	},
+	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),

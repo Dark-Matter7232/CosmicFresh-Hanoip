53599b2cdf9ad9592df72fc2109d10fe111cb519
Rick Yiu <rickyiu@google.com>
2020-07-11 11:45:54 +0800

    sched: fine tune task placement for prioritized tasks
    The prioritized task is defined as prefer high capacity with prio <=
    120. We found on the chipset with 2 big cores only, when one big core
    is occupied by busy non-prioritized tasks, prioritized tasks could be
    scheduled to the other big core. This may affect UX because the
    prioritized tasks are mostly UI related. Address this problem by put
    at least one prioritized task to the cpu which has no prioritized task
    running on it. Besides, if both big cores have prioritized tasks on them
    already, then schedule arriving prioritized tasks to idle little core if
    there is in the hope of not being blocked by existing prioritized tasks.
    
    Bug: 161190988
    Bug: 160663228
    Test: tasks scheduled as expected
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I880e3a68a5c8fbfd0902e1b571a83bfd88ce1b96

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2780,6 +2780,47 @@ static inline void account_numa_dequeue(struct rq *rq, struct task_struct *p)
 
 #endif /* CONFIG_NUMA_BALANCING */
 
+DEFINE_PER_CPU(unsigned long *, prioritized_task_mask);
+DEFINE_PER_CPU(int, prioritized_task_nr);
+
+static bool bitmap_testbit(unsigned long *map, unsigned long bit)
+{
+	if (*(map + BIT_WORD(bit)) & BIT_MASK(bit))
+		return true;
+
+	return false;
+}
+
+static void inc_prioritized_task_count(struct rq *rq, struct task_struct *p)
+{
+	bool bitset = bitmap_testbit(per_cpu(prioritized_task_mask, cpu_of(rq)),
+				     p->pid);
+
+	if (schedtune_prefer_high_cap(p) && p->prio <= DEFAULT_PRIO) {
+		if (likely(!bitset)) {
+			__bitmap_set(per_cpu(prioritized_task_mask, cpu_of(rq)),
+				     p->pid, 1);
+			per_cpu(prioritized_task_nr, cpu_of(rq))++;
+		}
+	} else if (unlikely(bitset)) {
+		__bitmap_clear(per_cpu(prioritized_task_mask, cpu_of(rq)),
+			       p->pid, 1);
+		if (per_cpu(prioritized_task_nr, cpu_of(rq)) > 0)
+			per_cpu(prioritized_task_nr, cpu_of(rq))--;
+	}
+}
+
+static void dec_prioritized_task_count(struct rq *rq, struct task_struct *p)
+{
+	if (bitmap_testbit(per_cpu(prioritized_task_mask, cpu_of(rq)),
+			   p->pid)) {
+		__bitmap_clear(per_cpu(prioritized_task_mask, cpu_of(rq)),
+			       p->pid, 1);
+		if (per_cpu(prioritized_task_nr, cpu_of(rq)) > 0)
+			per_cpu(prioritized_task_nr, cpu_of(rq))--;
+	}
+}
+
 static void
 account_entity_enqueue(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
@@ -2795,6 +2836,9 @@ account_entity_enqueue(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	}
 #endif
 	cfs_rq->nr_running++;
+	if (likely(entity_is_task(se))) {
+		inc_prioritized_task_count(rq_of(cfs_rq), task_of(se));
+	}
 }
 
 static void
@@ -2810,6 +2854,9 @@ account_entity_dequeue(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	}
 #endif
 	cfs_rq->nr_running--;
+	if (likely(entity_is_task(se))) {
+		dec_prioritized_task_count(rq_of(cfs_rq), task_of(se));
+	}
 }
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
@@ -5853,7 +5900,7 @@ cpu_is_in_target_set(struct task_struct *p, int cpu)
 	struct root_domain *rd = cpu_rq(cpu)->rd;
 	int first_cpu, next_usable_cpu;
 
-	if (schedtune_prefer_high_cap(p)) {
+	if (schedtune_prefer_high_cap(p) && p->prio <= DEFAULT_PRIO) {
 		first_cpu = rd->mid_cap_orig_cpu != -1 ? rd->mid_cap_orig_cpu :
 			    rd->max_cap_orig_cpu;
 
@@ -7420,7 +7467,8 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (is_min_capacity_cpu(cpu)) {
 		if (task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
 			task_boost > 0 ||
-			schedtune_prefer_high_cap(p))
+			(schedtune_prefer_high_cap(p) &&
+			 p->prio <= DEFAULT_PRIO))
 			return false;
 	} else { /* mid cap cpu */
 		if (task_boost > 1)
@@ -7476,7 +7524,7 @@ static int start_cpu(struct task_struct *p, bool boosted,
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
 	int start_cpu = -1;
 
-	if (boosted && p->prio <= DEFAULT_PRIO)) {
+	if (boosted && p->prio <= DEFAULT_PRIO) {
 		if (rd->mid_cap_orig_cpu != -1 &&
 		    task_fits_max(p, rd->mid_cap_orig_cpu))
 			return rd->mid_cap_orig_cpu;
@@ -7535,6 +7583,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	struct sched_group *sg;
 	int best_active_cpu = -1;
 	int best_idle_cpu = -1;
+	int best_prioritized_cpu = -1;
 	int target_cpu = -1;
 	int cpu, i;
 	long spare_wake_cap, most_spare_wake_cap = 0;
@@ -7545,6 +7594,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	int isolated_candidate = -1;
 	int mid_cap_orig_cpu = cpu_rq(smp_processor_id())->rd->mid_cap_orig_cpu;
 	struct task_struct *curr_tsk;
+	bool prioritized_task = prefer_high_cap && p->prio <= DEFAULT_PRIO;
 
 	*backup_cpu = -1;
 
@@ -7599,6 +7649,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long wake_util, new_util, new_util_cuml;
 			long spare_cap;
 			int idle_idx = INT_MAX;
+			bool best_prioritized_candidate;
 
 			trace_sched_cpu_util(i);
 
@@ -7656,7 +7707,12 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * capacity margin.
 			 */
 			new_util = max(min_util, new_util);
+			best_prioritized_candidate =
+				prioritized_task && !is_min_capacity_cpu(i) &&
+				per_cpu(prioritized_task_nr, i) == 0;
+
 			if ((!(prefer_idle && idle_cpu(i)) &&
+			     !best_prioritized_candidate &&
 			     new_util > capacity_orig) ||
 			    (is_min_capacity_cpu(i) &&
 			     !task_fits_capacity(p, capacity_orig, i)))
@@ -7744,6 +7800,11 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				}
 				if (best_idle_cpu != -1)
 					continue;
+
+				if (best_prioritized_candidate) {
+					best_prioritized_cpu = i;
+					continue;
+				}
 
 				/*
 				 * Case A.2: Target ACTIVE CPU
@@ -7912,9 +7973,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		 * accommodated in the higher capacity CPUs.
 		 */
 		if ((prefer_idle && best_idle_cpu != -1) ||
-		    (prefer_high_cap &&
-		     (best_idle_cpu != -1 || target_cpu != -1))) {
-			if (prefer_high_cap && p->prio <= DEFAULT_PRIO) {
+		    (prioritized_task && best_prioritized_cpu != -1) ||
+		    (prefer_high_cap && p->prio > DEFAULT_PRIO &&
+			 target_cpu != -1)) {
+			if (prioritized_task) {
 				/*
 				 * For prefer_high_cap task, stop searching when an idle
 				 * cpu is found in mid cluster.
@@ -7938,6 +8000,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 		return best_idle_cpu;
 	}
+
+	if (prioritized_task && (best_prioritized_cpu != -1))
+		return best_prioritized_cpu;
 
 	if (best_idle_cpu != -1 &&
 	    !is_packing_eligible(p, target_cpu, fbt_env, active_cpus_count,

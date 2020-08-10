c0a2d2f3df9c0a8a4665a647fcf6db4976778c53
Panchajanya1999 <panchajanya@azure-dev.live>
2020-08-11 03:12:32 +0530

    techpack: sm6150: Remove qos usage
    Change-Id: I69e6010b20f895faf61951d97e9028c8909bada7
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- techpack/audio/asoc/sm6150.c
+++ techpack/audio/asoc/sm6150.c
@@ -21,7 +21,6 @@
 #include <linux/module.h>
 #include <linux/input.h>
 #include <linux/of_device.h>
-#include <linux/pm_qos.h>
 #include <linux/soc/qcom/fsa4480-i2c.h>
 #include <sound/core.h>
 #include <sound/soc.h>
@@ -97,7 +96,6 @@ static struct snd_soc_codec_conf *msm_prince_codec_conf;
 #endif
 
 #define ADSP_STATE_READY_TIMEOUT_MS 3000
-#define MSM_LL_QOS_VALUE 300 /* time in us to ensure LPM doesn't go in C3/C4 */
 #define MSM_HIFI_ON 1
 
 #define SM6150_SOC_VERSION_1_0 0x00010000
@@ -6102,25 +6100,6 @@ static struct snd_soc_ops sm6150_tdm_be_ops = {
 };
 #endif
 
-static int msm_fe_qos_prepare(struct snd_pcm_substream *substream)
-{
-	if (pm_qos_request_active(&substream->latency_pm_qos_req))
-		pm_qos_remove_request(&substream->latency_pm_qos_req);
-
-	substream->latency_pm_qos_req.cpus_affine = BIT(1) | BIT(2);
-
-	substream->latency_pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
-
-	pm_qos_add_request(&substream->latency_pm_qos_req,
-			  PM_QOS_CPU_DMA_LATENCY,
-			  MSM_LL_QOS_VALUE);
-	return 0;
-}
-
-static struct snd_soc_ops msm_fe_qos_ops = {
-	.prepare = msm_fe_qos_prepare,
-};
-
 #ifdef CONFIG_SEC_MI2S_MODS
 static void msm_mi2s_path_enable(bool enable)
 {
@@ -6616,7 +6595,6 @@ static struct snd_soc_dai_link msm_common_dai_links[] = {
 		/* this dainlink has playback support */
 		.ignore_pmdown_time = 1,
 		.id = MSM_FRONTEND_DAI_MULTIMEDIA5,
-		.ops = &msm_fe_qos_ops,
 	},
 	{/* hw:x,14 */
 		.name = "Listen 1 Audio Service",
@@ -6683,7 +6661,6 @@ static struct snd_soc_dai_link msm_common_dai_links[] = {
 		.ignore_pmdown_time = 1,
 		 /* this dainlink has playback support */
 		.id = MSM_FRONTEND_DAI_MULTIMEDIA8,
-		.ops = &msm_fe_qos_ops,
 	},
 	/* HDMI Hostless */
 	{/* hw:x,18 */

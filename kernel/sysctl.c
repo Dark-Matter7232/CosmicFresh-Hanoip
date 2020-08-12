78532a8f3b29bb772e45fdfa4766bbdabcb2895e
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-08-12 09:36:11 +0530

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Ia728ecbc780c9d172b49029ef3c87c6b3b490872
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -575,6 +575,15 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &max_cfs_boost_prio,
 	},
 	{
+		.procname	= "walt_low_latency_task_boost",
+		.data		= &sysctl_walt_low_latency_task_boost,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),

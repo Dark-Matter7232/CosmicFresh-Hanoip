bf06cdba5dc669ba899c069c55a97a5ce91195e8
Biao Long <blong@codeaurora.org>
2020-09-02 16:52:05 +0800

    sched/fair: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I917955ef029f30db6c185a41f717a89dd179cc49
    Signed-off-by: Biao Long <blong@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -551,6 +551,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_updown_migrate_handler,
 	},
 	{
+		.procname	= "sched_force_lb_enable",
+		.data		= &sysctl_sched_force_lb_enable,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),

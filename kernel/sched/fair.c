69866732cf50ed617181b6cf86be09e10143fe1b
Diep Quynh <remilia.1505@gmail.com>
2021-04-17 21:06:42 +0700

    sched: fair: Disallow prefer_high_cap tasks to run on smallest cluster
    These tasks are marked as latency sensitive, therefore don't allow them
    to be balanced to CPUs with smallest capacity
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7417,7 +7417,8 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 
 	if (is_min_capacity_cpu(cpu)) {
 		if (task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
-			task_boost > 0)
+			task_boost > 0 ||
+			schedtune_prefer_high_cap(p))
 			return false;
 	} else { /* mid cap cpu */
 		if (task_boost > 1)

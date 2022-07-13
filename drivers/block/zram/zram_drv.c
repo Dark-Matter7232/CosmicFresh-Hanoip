b16d4293aca6c61f6fe24bb89f4dff5183954343
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-07-13 06:24:00 +0530

    Revert "zram: Expose disksize as a config option"
    This reverts commit 560a4d7c189f6437a0a292b2c5c44ef8c2e5976b.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1809,7 +1809,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)CONFIG_ZRAM_DISKSIZE * SZ_1M;
+	disksize = (u64)2888 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 

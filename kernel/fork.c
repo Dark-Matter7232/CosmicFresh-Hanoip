b2aaf5ae92b612aaa89c3e9afd0c6e83739b3f25
Dark-Matter7232 <me@const.eu.org>
2022-09-11 22:15:45 +0530

    Revert "treewide: drop CIB"
    This reverts commit 1b3f05b34c87d9b214d215f322b2ede81f00b623.

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
@@ -2250,8 +2251,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
+	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		cpu_input_boost_kick_max(50);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
